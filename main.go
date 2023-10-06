2e930b2645cd42f485be16abe0b7882754bcc437
iovsomnium <ksw09157.5212@gmail.com>
2023-10-06 11:31:08 +0900

    feat: now listen with 8991 port, return result

--- main.go
+++ main.go
@@ -1,105 +1,48 @@
 package main
 
 import (
-	"flag"
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var (
-	bitcoinBlocks = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "bitcoin_blocks",
-		Help: "Block height",
-	})
-	bitcoinDifficulty = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "bitcoin_difficulty",
-		Help: "Difficulty",
-	})
-	bitcoinPeers = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "bitcoin_peers",
-		Help: "Number of peers",
-	})
-	bitcoinConnIn = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "bitcoin_conn_in",
-		Help: "Number of connections in",
-	})
-	bitcoinConnOut = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "bitcoin_conn_out",
-		Help: "Number of connections out",
-	})
-
-	bitcoinHashPS = make(map[int]prometheus.Gauge)
-	bitcoinEstSmartFee = make(map[int]prometheus.Gauge)
-
-	retryExceptions = []string{
-		"btcrpcclient.InWarmupError",
-		"btcrpcclient.JSONRPCError",
-		"btcrpcclient.BTCDRPCError",
-	}
-
-	bitcoinRPCUser   string
-	bitcoinRPCPass   string
-	bitcoinRPCServer string
-)
-
-func init() {
-	// Register all metrics with the Prometheus client library
-	prometheus.MustRegister(bitcoinBlocks)
-	prometheus.MustRegister(bitcoinDifficulty)
-	prometheus.MustRegister(bitcoinPeers)
-	prometheus.MustRegister(bitcoinConnIn)
-	prometheus.MustRegister(bitcoinConnOut)
-
-	for _, numBlocks := range []int{-1, 1, 120} {
-		bitcoinHashPS[numBlocks] = prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: fmt.Sprintf("bitcoin_hashps_%d", numBlocks),
-			Help: fmt.Sprintf("Estimated network hash rate per second for the last %d blocks", numBlocks),
-		})
-		prometheus.MustRegister(bitcoinHashPS[numBlocks])
-	}
-
-	for _, numBlocks := range []int{2, 3, 5, 20} {
-		bitcoinEstSmartFee[numBlocks] = prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: fmt.Sprintf("bitcoin_est_smart_fee_%d", numBlocks),
-			Help: fmt.Sprintf("Estimated smart fee per kilobyte for confirmation in %d blocks", numBlocks),
-		})
-		prometheus.MustRegister(bitcoinEstSmartFee[numBlocks])
-	}
-
-	// Add other metrics as per the Python code
-	// ...
+type Response struct {
+	Result int         `json:"result"`
+	Error  interface{} `json:"error"`
+	ID     string      `json:"id"`
 }
 
 func main() {
-	// Parse command-line flags for Bitcoin RPC settings
-	flag.StringVar(&bitcoinRPCUser, "rpcuser", "", "Bitcoin RPC username")
-	flag.StringVar(&bitcoinRPCPass, "rpcpass", "", "Bitcoin RPC password")
-	flag.StringVar(&bitcoinRPCServer, "rpcserver", "localhost:8332", "Bitcoin RPC server address")
-	flag.Parse()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Only GET requests are allowed", http.StatusMethodNotAllowed)
+			return
+		}
 
-	// Set up Prometheus metrics handler
-	http.Handle("/metrics", promhttp.Handler())
+	    url := "http://127.0.0.1:8332"
 
-	// Gracefully handle SIGTERM to shut down the HTTP server
-	signalChannel := make(chan os.Signal, 1)
-	signal.Notify(signalChannel, syscall.SIGTERM)
-	go func() {
-		<-signalChannel
-		log.Println("Received SIGTERM. Exiting...")
-		os.Exit(0)
-	}()
+		payload := []byte(`{"jsonrpc": "1.0", "id": "curltest", "method": "getblockcount", "params": []}`)
 
-	// Start the HTTP server to serve metrics
-	log.Println("Starting HTTP server on :9332")
-	log.Fatal(http.ListenAndServe(":9332", nil))
+		resp, err := http.Post(url, "application/json", bytes.NewBuffer(payload))
+		if err != nil {
+			http.Error(w, "Error making POST request", http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		var responseData Response
+		decoder := json.NewDecoder(resp.Body)
+		err = decoder.Decode(&responseData)
+		if err != nil {
+			http.Error(w, "Error decoding JSON resp", http.StatusInternalServerError)
+			return
+		}
+
+		result := responseData.Result
+		fmt.Fprintf(w, "bitcoin_latest_block_height %d\n", result)
+	})
+
+	fmt.Println("Server listening on port 8991 for GET requests...")
+	http.ListenAndServe(":8991", nil)
 }
-
-// Add other metric collection functions as per the Python code
-// ...
